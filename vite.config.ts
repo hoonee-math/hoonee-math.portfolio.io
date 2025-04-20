2e177571c97cc81e5fa535929b8dd978cf8baebe
HOONEE_LENOVO\kwang <kwanghoon041@gmail.com>
2025-04-20 09:30:02 +0900

    Fix: npm install gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/hoonee-math.portfolio.io',
+  base: '/hoonee-math.portfolio.io/',
     build: {
       outDir: 'docs'
     },
